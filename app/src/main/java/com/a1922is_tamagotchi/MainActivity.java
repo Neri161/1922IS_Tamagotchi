cf797f6deac164f64a0707e57f1ce3ac59cb88fb
Neri161 <Neri010601>
2022-05-25 03:28:42 -0500

    back-end vista principal

--- app/src/main/java/com/a1922is_tamagotchi/MainActivity.java
+++ app/src/main/java/com/a1922is_tamagotchi/MainActivity.java
@@ -5,18 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public SharedPreferences configurar;
     public TextView prueba;
+    public EditText txtNombre;
+    public Button btnComenzar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        prueba=(TextView) findViewById(R.id.prueba);
+       // prueba=(TextView) findViewById(R.id.prueba);
+        txtNombre = (EditText)findViewById(R.id.etNombre);
+        btnComenzar = (Button) findViewById(R.id.btnSiguiente);
+        btnComenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+            }
+        });
+
         consumoApi apis = new consumoApi();
         String temp=apis.accederApi("POST","name","results");
         prueba.setText(temp);
